b15841ef63b181cda694e306d16506a38658556a
melvin-leo <mel5469leo@hotmail.com>
2019-03-28 17:44:25 +0800

    Added ML prediction to Rpi

--- comms/RPi.py
+++ comms/RPi.py
@@ -9,11 +9,31 @@ import arduino as my_Ard
 import csv
 from Crypto import Random
 from Crypto.Cipher import AES
+from keras.models import load_model
+import numpy as np
 
 
 #global variables
 dataQueue = queue.Queue(1000)
 queueLock = threading.Lock()
+labels_dict = {
+    0: 'hunch', 1: 'cowboy', 2: 'crab', 3: 'chicken', 4: 'raffles'
+}
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+model_path = os.path.join(PROJECT_DIR, 'models', 'firstmodel.h5')
+model = load_model(model_path)
+n_features = 18
+# reshape data into time steps of sub-sequences
+n_steps, n_length = 4, 15
+# for i in range(len(test_samples)):
+#     test_samples[i] = test_samples[i].reshape((1, n_steps, n_length, n_features))
+
+
+def normalise(x, minx, maxx):
+    new_x = 2*(x-minx)/(maxx-minx) - 1
+    return new_x
 
 
 def Main_Run():
@@ -37,6 +57,7 @@ class toMLtoServer(threading.Thread):
         voltage = ""
         current = ""
         cumpower = ""
+        ml_data = []
         while True:
             queueLock.acquire()
             if not dataQueue.empty(): #check if queue is empty or not. If empty, dont try to take from queue
@@ -46,9 +67,41 @@ class toMLtoServer(threading.Thread):
                 voltage = packet_data["voltage"]
                 current = packet_data["current"]
                 cumpower = packet_data["cumpower"]
-                danceMove = my_ML.give(packet_data)
+                ml_data.append(packet_data["01"] + packet_data["02"] + packet_data["03"])
             queueLock.release()
-            #danceMove = my_ML.give(packet_data["01"] + packet_data["02"] + packet_data["03"]) #dummy class for sending
+            #ML prediction
+            if len(ml_data) == 60:
+                for i in range(len(ml_data)):
+                    if i < 3:
+                        ml_data[i] = normalise(ml_data[i], -2000, 2000)
+                    elif i in range(3, 6):
+                        ml_data[i] = normalise(ml_data[i], -250000, 250000)
+                    elif i in range(6, 9):
+                        ml_data[i] = normalise(ml_data[i], -2000, 2000)
+                    elif i in range(9, 12):
+                        ml_data[i] = normalise(ml_data[i], -250000, 250000)
+                    elif i in range(12, 15):
+                        ml_data[i] = normalise(ml_data[i], -2000, 2000)
+                    elif i in range(15, 18):
+                        ml_data[i] = normalise(ml_data[i], -250000, 250000)
+                arr_data = []
+                for array in ml_data:
+                    arr_raw = []
+                    arr_raw += [
+                        array[0], array[1], array[2], array[6], array[7], array[8], array[12], array[13], array[14],
+                        array[3],
+                        array[4], array[5], array[9], array[10], array[11], array[15], array[16], array[17]
+                    ]
+                    arr_data.append(arr_raw)
+
+                test_sample = arr_data
+                test_sample = np.array(test_sample)
+                test_sample = test_sample.reshape(1, n_steps, n_length, n_features)
+                print(test_sample.shape)
+                result = model.predict(test_sample, batch_size=96, verbose=0)
+                result_int = int(np.argmax(result[0]))
+                danceMove = labels_dict[result_int]
+                ml_data = []
             data = Data(my_pi.sock)
             data.sendData(danceMove, power, voltage, current, cumpower)
             #time.sleep(2)
